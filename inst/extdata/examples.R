98d201bf095ff21b378417efcb9fafcad9bf02da
Michael Sumner <mdsumner@gmail.com>
2014-03-26 14:58:29 +1100

    readme for install and test case

---
+++ inst/extdata/examples.R
@@ -0,0 +1,6 @@
+## Mike is testing the synchronization on home computer
+## with config in GIT/repomania/inst/extdata/raad_repo_config.json
+
+## 2014-03-26
+## "SMMR-SSM/I Nasateam daily sea ice concentration
+## do_sync set to true, sync_from_year set to 2014
